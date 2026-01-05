9642f7e9eb3cd2bf5407442cbbb7366731e0792c
pauline-finnovate <pauline@finnovate.io>
2026-01-05 15:05:50 -0500

    feat: prizes page

---
+++ src/api/prize-page/controllers/prize-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * prize-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::prize-page.prize-page');
